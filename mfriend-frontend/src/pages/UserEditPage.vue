d9ec7f28dcebc3038279f40b93c0929bbeec69c0
hskwindow <hskwindow@gmail.com>
2024-06-18 15:44:36 +0800

    add 用户编辑页

---
+++ mfriend-frontend/src/pages/UserEditPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <van-form @submit="onSubmit">
+    <van-cell-group inset>
+      <van-field
+          v-model="editUser.currentValue"
+          :name="editUser.editKey"
+          :label="editUser.editName"
+          :placeholder="`请输入${editUser.editName}`"
+      />
+
+    </van-cell-group>
+    <div style="margin: 16px;">
+      <van-button round block type="primary" native-type="submit">
+        提交
+      </van-button>
+    </div>
+  </van-form>
+
+</template>
+
+<script setup>
+import {ref} from "vue";
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+
+const editUser = ref({
+  editKey: route.query.editKey,
+  currentValue: route.query.currentValue,
+  editName: route.query.editName,
+});
+
+//todo 联调后端提交接口
+const onSubmit = (values) => {
+  console.log('submit', values);
+};
+
+</script>
+
+<style scoped>
+
+</style>
